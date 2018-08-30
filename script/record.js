dd3c247a212b4df82b62fe54ad3682b2ba9e33ed
yoyoyousei <atom.soichi0407@gmail.com>
2018-08-30 20:57:03 +0900

    initial

---
+++ script/record.js
@@ -0,0 +1,47 @@
+let fs = require("fs");
+let path = require("path");
+let rp = require("request-promise");
+
+const endpoint = "https://us-central1-fir-test-2fc32.cloudfunctions.net/helloWorld";
+const repeat = 1;
+
+function writeLine(filename, line){
+    let filePath = path.join(__dirname, filename);
+    fs.appendFileSync(filePath, line+'\n', (err) => {
+        console.log(`err: ${err}`);
+    });
+}
+const resFileName = 'result.csv'
+const resFilePath = path.join(__dirname, resFileName);
+
+async function callFunction(){
+    const options = {
+        uri: endpoint,
+        body: {
+            message: "test",
+        },
+        json: true,
+    };
+    const t1 = new Date().getMilliseconds();
+    const res = await rp(options);
+    console.log(res)
+    const t2 = new Date().getMilliseconds();
+    return t2-t1;
+}
+
+async function testFunction(){
+    for(let i = 0; i < repeat; i++){
+        try{
+            const msec = await callFunction();
+            writeLine(`${i},${msec}`);
+        }catch(err){
+            console.log(err);
+        }
+    }
+}
+
+testFunction().then((res)=>{
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+});
